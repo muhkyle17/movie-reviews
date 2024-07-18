4bc895687cb4e9475d95e98691da39f0133e86d6
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-07-18 15:28:23 -0700

    Modify the server.js to better fit the most updated connection

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,20 @@
-import express from 'express'
-import cors from 'cors'
-import movies from './api/movies.route.js'
+import express from "express";
+import cors from "cors";
+import movies from "./api/movies.route.js";
 
 // Create the server
-const app = express()
+const app = express();
+const PORT = process.env.PORT || 8000;
 
-// Attach cors and express.json middleware that express will use
-app.use(cors())
-app.use(express.json()) // JSON parsing middleware to enable the server to read and accept JSON in a request's body
+app.use(cors());
+app.use(express.json()); // JSON parsing middleware to enable the server to read and accept JSON in a request's body
+app.use("/api/v1/movies", movies);
 
-app.use('/api/v1/movies', movies)
-app.use('*', (req, res) => {
-  res.status(404).json({ error: 'not found' })
-})
+app.use("*", (req, res) => {
+  res.status(404).json({ error: "not found" });
+});
 
-export default app
+// start the Express server
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
